b7637dd8ac0ec55d038cd401f0c0f5348dc3d1e5
Hoang Dang <hoangdang.eng@gmail.com>
2026-06-19 14:20:37 -0400

    update some content and also update to new dependencies for deprecation of node.js 20

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://hoangdang.dev',
     base: '',
-    integrations: [tailwind(), 
-        icon()
-    ],
+    integrations: [tailwind(), sitemap(), icon()],
 });
